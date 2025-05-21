0a23f6d385a95c69967da2b7996de53f0d9ec047
“boar616666” <gearssgt@gmail.com>
2025-05-20 18:52:05 -0600

    Version 1

---
+++ src/componentes/App.jsx
@@ -0,0 +1,17 @@
+import NavBar from "./NavBar"; 
+function App() {
+    return (
+        <>
+            <NavBar />  
+            <div>
+                <h2>Bienvenido a mi página web</h2>
+                <hr />
+                <p>
+                    
+                </p>
+            </div>
+        </>
+    );
+}
+
+export default App;
